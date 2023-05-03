3027e96c988ae3b5915d2cfe4ebd8ad99017cc86
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-05-03 20:33:13 +0300

    header created

---
+++ src/header/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './header.module.css'
+
+const Header = () => {
+    return (
+        <div className={s.header}>
+
+        </div>
+    );
+};
+
+export default Header;
